14e3e2fad2df54dd24dddce942ccaa9576fb032d
Winston Chu <wpchu@andrew.cmu.edu>
2017-03-30 18:37:45 -0400

    fixed problem with smooth scroll page jump and modals not working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -102,3 +102,17 @@ $(document).ready(function(){
 
 // Smooth scrolling when page jumps
 
+$(function() {
+  $('a[href="#questionnaire"]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
