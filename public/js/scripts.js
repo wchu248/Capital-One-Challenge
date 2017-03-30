80a6f5ff0b2b107140cc85f04b5afdcad47a0ee6
Winston Chu <wpchu@andrew.cmu.edu>
2017-03-30 18:05:14 -0400

    fixed broken links

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -102,17 +102,3 @@ $(document).ready(function(){
 
 // Smooth scrolling when page jumps
 
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
